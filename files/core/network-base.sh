1a3e9a23135ec092e60ca304a3a361418692009e
Stu Franks <stu.j.franks@gmail.com>
2017-08-30 11:49:05 +0100

    Removed resolv.conf if statement and firewall disabling

--- files/core/network-base.sh
+++ files/core/network-base.sh
@@ -9,16 +9,7 @@ service NetworkManager stop
 echo "HOSTNAME=<%= networks.pri.hostname %>" >> /etc/sysconfig/network
 echo "<%= networks.pri.hostname %>" > /etc/hostname
 
-systemctl disable firewalld
-
-#if ! [ "<%= profile %>" = "MASTER" ]; then
 cat << EOF > /etc/resolv.conf
 search <%= search_domains %>
 nameserver <%= internaldns %>
 EOF
-#else
-#cat << EOF > /etc/resolv.conf
-#search <%= search_domains %>
-#nameserver <%= externaldns %>
-#EOF
-#fi
