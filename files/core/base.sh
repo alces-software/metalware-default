dc3714770ac9954362af83c4dbd0e3dd69a9772c
Stu Franks <stu.j.franks@gmail.com>
2017-08-31 14:56:02 +0100

    Added firewall policies and timeserver

--- files/core/base.sh
+++ files/core/base.sh
@@ -25,3 +25,57 @@ if [ "<%= profile %>" == 'INFRA' ]; then
 else
   echo "Unrecognised profile"
 fi
+
+# NTP/Chrony
+yum -y install chrony
+<% if ntp.is_server -%>
+cat << EOF > /etc/chrony.conf
+server 0.centos.pool.ntp.org iburst
+server 1.centos.pool.ntp.org iburst
+server 2.centos.pool.ntp.org iburst
+server 3.centos.pool.ntp.org iburst
+
+stratumweight 0
+
+driftfile /var/lib/chrony/drift
+
+rtcsync
+
+makestep 10 3
+
+bindcmdaddress 127.0.0.1
+bindcmdaddress ::1
+
+keyfile /etc/chrony.keys
+
+commandkey 1
+
+generatecommandkey
+
+noclientlog
+
+logchange 0.5
+
+logdir /var/log/chrony
+
+allow <%= networks.pri.network %>/<% require 'ipaddr'; puts IPAddr.new(networks.pri.netmask).to_i.to_s(2).count('1') %>
+EOF
+<% else -%>
+cat << EOF > /etc/chrony.conf
+tinker panic 0
+
+restrict default kod nomodify notrap nopeer noquery
+restrict -6 default kod nomodify notrap nopeer noquery
+restrict 127.0.0.1
+restrict -6 ::1
+
+server <%= ntp.server %> iburst
+server 0.centos.pool.ntp.org iburst
+server 1.centos.pool.ntp.org iburst
+server 2.centos.pool.ntp.org iburst
+
+driftfile /var/lib/ntp/drift
+EOF
+<% end -%>
+systemctl start chronyd
+systemctl enable chronyd
--- files/core/network-base.sh
+++ files/core/network-base.sh
@@ -9,7 +9,45 @@ service NetworkManager stop
 echo "HOSTNAME=<%= networks.pri.hostname %>" >> /etc/sysconfig/network
 echo "<%= networks.pri.hostname %>" > /etc/hostname
 
+<% if dns_type == 'named' || alces.nodename != 'self'  %>
 cat << EOF > /etc/resolv.conf
 search <%= search_domains %>
 nameserver <%= internaldns %>
 EOF
+<% else %>
+cat << EOF > /etc/resolv.conf
+search <%= search_domains %>
+nameserver <%= externaldns %>
+EOF
+<% end %>
+
+<% if firewall.enabled -%>
+systemctl enable firewalld
+systemctl start firewalld
+
+<%     firewall.each do |zone, info| -%>
+<%     next if zone.to_s == 'enabled' -%>
+# Create zone
+firewall-cmd --info-zone=<%= zone %> >> /dev/null
+if [ $? != 0 ] ; then
+    firewall-cmd --new-zone <%= zone %> --permanent
+fi
+# Add services
+<%         info.services.split(' ').each do |service| -%>
+firewall-cmd --add-service <%= service %> --zone <%= zone %> --permanent
+<%         end -%>
+
+<%     end -%>
+firewall-cmd --reload
+
+# Add interfaces to zones
+<%     networks.each do |network, info| -%>
+<%         if info.defined -%>
+firewall-cmd --add-interface <%= info.interface %> --zone <%= info.firewallpolicy %> --permanent
+<%         end -%>
+<%     end -%>
+
+<% else -%>
+systemctl disable firewalld
+systemctl stop firewalld
+<% end -%>
