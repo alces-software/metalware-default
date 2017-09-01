52ba97da2e8e42e57ed05da00917c24c99c6bd61
Stu Franks <stu.j.franks@gmail.com>
2017-09-01 18:22:03 +0100

    Added gateway and dns config

---
+++ files/setup/dns.sh
@@ -0,0 +1,49 @@
+<% if dns_type == 'named' then %>
+yum -y install bind bind-utils
+
+cat << EOF > /etc/named.conf
+options {
+          listen-on port 53 { any; };
+          directory       "/var/named";
+          dump-file       "/var/named/data/cache_dump.db";
+          statistics-file "/var/named/data/named_stats.txt";
+          memstatistics-file "/var/named/data/named_mem_stats.txt";
+          allow-query     { any; };
+          recursion yes;
+
+
+          dnssec-enable no;
+          dnssec-validation no;
+          dnssec-lookaside auto;
+
+          forward first;
+          forwarders {
+              <%= externaldns %>;
+          };
+
+};
+
+logging {
+        channel default_debug {
+                file "data/named.run";
+                severity dynamic;
+        };
+};
+
+include "/etc/named/metalware.conf";
+EOF
+
+touch /etc/named/metalware.conf
+
+systemctl stop dnsmasq
+systemctl disable dnsmasq
+
+systemctl start named
+systemctl enable named
+<% else %>
+systemctl stop named
+systemctl disable named
+
+systemctl start dnsmasq
+systemctl enable dnsmasq
+<% end %>
