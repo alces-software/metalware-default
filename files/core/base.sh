e8338a925154f3745c57e9c64679f6d705900a28
Stu Franks <stu.j.franks@gmail.com>
2017-12-08 14:39:41 +0000

    Remove centos NTP servers, add first boot and postfix setup, tweak firewall rules for rsyslog to firstrun

--- files/core/base.sh
+++ files/core/base.sh
@@ -67,15 +67,69 @@ EOF
 <% else -%>
 cat << EOF > /etc/chrony.conf
 server <%= config.ntp.server %> iburst
-server 0.centos.pool.ntp.org iburst
-server 1.centos.pool.ntp.org iburst
-server 2.centos.pool.ntp.org iburst
+
+makestep 360 10
 
 driftfile /var/lib/ntp/drift
 EOF
 <% end -%>
 systemctl start chronyd
 systemctl enable chronyd
+
+# First boot
+mkdir -p /var/lib/firstrun/{bin,scripts}
+mkdir -p /var/log/firstrun/
+
+cat << EOF > /var/lib/firstrun/bin/firstrun
+#!/bin/bash
+function fr {
+  echo "-------------------------------------------------------------------------------"
+  echo "Symphony deployment Suite - Copyright (c) 2008-2017 Alces Software Ltd"
+  echo "-------------------------------------------------------------------------------"
+  echo "Running Firstrun scripts.."
+  if [ -f /var/lib/firstrun/RUN ]; then
+    for script in \`find /var/lib/firstrun/scripts -type f -iname *.bash\`; do
+      echo "Running \$script.." >> /root/firstrun.log 2>&1
+      /bin/bash \$script >> /root/firstrun.log 2>&1
+    done
+    rm -f /var/lib/firstrun/RUN
+  fi
+  echo "Done!"
+  echo "-------------------------------------------------------------------------------"
+}
+trap fr EXIT
+EOF
+
+cat << EOF > /var/lib/firstrun/bin/firstrun-stop
+#!/bin/bash
+/bin/systemctl disable firstrun.service
+if [ -f /firstrun.reboot ]; then
+  echo -n "Reboot flag set.. Rebooting.."
+  rm -f /firstrun.rebooot
+  shutdown -r now
+fi
+EOF
+
+cat << EOF >> /etc/systemd/system/firstrun.service
+[Unit]
+Description=FirstRun service
+After=network-online.target remote-fs.target
+Before=display-manager.service getty@tty1.service
+[Service]
+ExecStart=/bin/bash /var/lib/firstrun/bin/firstrun
+Type=oneshot
+ExecStartPost=/bin/bash /var/lib/firstrun/bin/firstrun-stop
+SysVStartPriority=99
+TimeoutSec=0
+RemainAfterExit=yes
+[Install]
+WantedBy=multi-user.target
+EOF
+
+chmod 664 /etc/systemd/system/firstrun.service
+systemctl daemon-reload
+systemctl enable firstrun.service
+touch /var/lib/firstrun/RUN
 
 # Syslog
 yum -y install rsyslog
@@ -102,12 +156,24 @@ cat << EOF > /etc/logrotate.d/rsyslog-remote
     endscript
 }
 EOF
+cat << EOF > /var/lib/firstrun/scripts/firewall_rsyslog.bash
 firewall-cmd --add-port 514/udp --zone internal --permanent
 firewall-cmd --add-port 514/tcp --zone internal --permanent
-fiewall-cmd --reload
+firewall-cmd --reload
+EOF
 <% else -%>
 echo '*.* @<%= config.rsyslog.server %>:514' >> /etc/rsyslog.conf
 <% end -%>
 
 systemctl enable rsyslog
 systemctl restart rsyslog
+
+# Postfix setup
+yum -y install postfix
+<% unless node.name == 'local' -%>
+sed -n -e '/^relayhost\s*=/!p' -e '$arelayhost=[<%=config.postfix.relayhost%>]' /etc/postfix/main.cf -i
+<% end -%>
+systemctl enable postfix
+systemctl restart postfix
+
+
